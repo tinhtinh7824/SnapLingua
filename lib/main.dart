2557bd681a4701cd88e8220292b2e4e8e3780d1f
Nguyễn Công Tỉnh <congtinh07082004@gmail.com>
2025-08-19 21:01:47 +0700

    update

--- lib/main.dart
+++ lib/main.dart
@@ -16,26 +16,70 @@ void main() {
 class MyWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: SizedBox(
-        width: 300,
-        height: double.infinity,
-        child: ElevatedButton(
-          onPressed: () {},
-          style:
-              ElevatedButton.styleFrom(
-                backgroundColor:
-                    Colors.green,
-                foregroundColor:
-                    Colors.white,
+    return Container(
+      color: Colors.orange,
+      child: Row(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment:
+            MainAxisAlignment
+                .spaceEvenly,
+        crossAxisAlignment:
+            CrossAxisAlignment.center,
+        children: [
+          ElevatedButton(
+            onPressed: () {},
+            style:
+                ElevatedButton.styleFrom(
+                  backgroundColor:
+                      Colors.green,
+                  foregroundColor:
+                      Colors.white,
+                ),
+            child: const Text(
+              "Button 1",
+              style: TextStyle(
+                fontSize: 20,
               ),
-          child: Text(
-            'Hello',
-            style: TextStyle(
-              fontSize: 30,
             ),
           ),
-        ),
+
+          Container(
+            height: 100,
+            child: ElevatedButton(
+              onPressed: () {},
+              style:
+                  ElevatedButton.styleFrom(
+                    backgroundColor:
+                        Colors.green,
+                    foregroundColor:
+                        Colors.white,
+                  ),
+              child: const Text(
+                "Button 2",
+                style: TextStyle(
+                  fontSize: 20,
+                ),
+              ),
+            ),
+          ),
+
+          ElevatedButton(
+            onPressed: () {},
+            style:
+                ElevatedButton.styleFrom(
+                  backgroundColor:
+                      Colors.green,
+                  foregroundColor:
+                      Colors.white,
+                ),
+            child: const Text(
+              "Button 3",
+              style: TextStyle(
+                fontSize: 20,
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
