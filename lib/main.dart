a7d2b87a0feaa32fe30d6822a1423da3ee4b38b5
Nguyễn Công Tỉnh <congtinh07082004@gmail.com>
2025-08-20 20:46:51 +0700

    update

--- lib/main.dart
+++ lib/main.dart
@@ -16,30 +16,25 @@ void main() {
 class MyWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 200,
-      height: 200,
-      padding: const EdgeInsets.all(25),
-      margin: const EdgeInsets.all(20),
-      alignment: Alignment.center,
-
-      decoration: BoxDecoration(
-        color: Colors.blue,
-        shape: BoxShape.rectangle,
-        borderRadius: BorderRadius.all(
-          Radius.circular(20),
-        ),
-        border: Border.all(
-          width: 2,
-          color: Colors.red,
-        ),
-      ),
-      transform: Matrix4.rotationZ(0.2),
-      child: const Text(
-        "data",
-        style: TextStyle(
-          fontSize: 30,
-          color: Colors.white,
+    return Center(
+      child: SizedBox(
+        width: 300,
+        height: double.infinity,
+        child: ElevatedButton(
+          onPressed: () {},
+          style:
+              ElevatedButton.styleFrom(
+                backgroundColor:
+                    Colors.green,
+                foregroundColor:
+                    Colors.white,
+              ),
+          child: Text(
+            'Hello',
+            style: TextStyle(
+              fontSize: 30,
+            ),
+          ),
         ),
       ),
     );
